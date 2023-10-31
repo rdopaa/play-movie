1bd5249ce964fdccfc289ea1559950f9371547cf
fxDopa <131190297+rdopaa@users.noreply.github.com>
2023-10-31 18:21:45 -0300

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://mainbweb.sfxdopa.workers.dev",
+  VITE_TMDB_READ_API_KEY: "bd27f477418d7def4f43040a9f1b280f"
+};
